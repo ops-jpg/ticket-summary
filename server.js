65dcaf39af8df4cbb25abb5f633be4a7df35c90c
ops-jpg <ops@adit.com>
2025-11-26 17:55:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -565,7 +565,7 @@ Category: Analytics Issue
 Category: Appointment Issue
 - Unable to book appointment: Appointment booking fails or throws an error.
 - Appointment not syncing: Appointment not showing in Adit or EHR.
-
+;
 
 // ------------ PROMPT (includes time per user & role + issue_summary) ------------
 const PROMPT = ({
